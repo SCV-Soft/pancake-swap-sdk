2c4809b9a79990bb973b7a20dd871571aa73d08a
benjioh5 <benjioh5@gmail.com>
2021-12-30 02:38:40 +0900

    update dist

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -3,7 +3,8 @@ export declare type BigintIsh = JSBI | bigint | string;
 export declare enum ChainId {
     MAINNET = 56,
     TESTNET = 97,
-    RINKEBY = 4
+    RINKEBY = 4,
+    HOMESTEAD = 1,
 }
 export declare enum TradeType {
     EXACT_INPUT = 0,
